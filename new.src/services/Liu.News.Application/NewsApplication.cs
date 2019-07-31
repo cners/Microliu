e9a6aa581b95d53c2ab3ee6ef798bdfa31d9ba3e
LiuZhuang <liuzhuang@6iuu.com>
2019-07-31 19:50:52 +0800

    test

---
+++ new.src/services/Liu.News.Application/NewsApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liu.News.Application
+{
+    [ModuleName("")]
+  public  class NewsApplication
+    {
+    }
+}
---
+++ new.src/services/Liu.News.IApplication/INewsApplication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liu.News.IApplication
+{
+    public interface INewsApplication
+    {
+    }
+}
