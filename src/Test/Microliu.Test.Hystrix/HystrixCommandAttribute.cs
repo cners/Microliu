4cfae6613520e9004b1e833fb67f76aaaccffd1c
LiuZhuang <liuzhuang@6iuu.com>
2019-08-06 19:49:27 +0800

    细化框架进行中

---
+++ src/Test/Microliu.Test.Hystrix/HystrixCommandAttribute.cs
@@ -0,0 +1,33 @@
+﻿using AspectCore.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microliu.Test.Hystrix
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HystrixCommandAttribute : AbstractInterceptorAttribute
+    {
+        public string FallBackMethod { get; set; }
+        public HystrixCommandAttribute(string fallBackMethod)
+        {
+            this.FallBackMethod = fallBackMethod;
+        }
+
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                var fallBackMethod = context.ServiceMethod.DeclaringType.GetMethod(this.FallBackMethod);
+                object fallBackResult = fallBackMethod.Invoke(context.Implementation, context.Parameters);
+                context.ReturnValue = fallBackResult;
+                await Task.FromResult(0);
+            }
+        }
+    }
+}
---
+++ src/Test/Microliu.Test.Hystrix/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microliu.Test.Hystrix
+{
+    public class Person
+    {
+        [HystrixCommand(nameof(HelloFallBackAsync))]
+        public virtual async Task<string> HelloAsync(string name)
+        {
+            Console.WriteLine($"hello {name}");
+
+            string s = null;
+            s.ToString();
+            return "ok";
+        }
+
+        public async Task<string> HelloFallBackAsync(string name)
+        {
+            Console.WriteLine($"执行失败：" + name);
+            return "fail";
+        }
+
+        [HystrixCommand(nameof(AddFall))]
+        public virtual int Add(int a, int b)
+        {
+            string s = null;
+            s.ToString();
+
+            return a + b;
+        }
+
+        public int AddFall(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
