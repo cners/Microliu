a299f1c50b397d4703301494cd7a5e8e39700c19
LiuZhuang <liuzhuang@6iuu.com>
2019-09-19 20:00:12 +0800

    增加utlis helper

--- src/Microliu.Utils/Microliu.Utils/Model/ReturnResult.cs
+++ src/Microliu.Utils/Microliu.Utils/Model/ReturnResult.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Microliu.EmailService.API.Models
+﻿namespace Microliu.Utils
 {
     public class ReturnResult
     {
--- src/Services/EmailService/Microliu.EmailService/Extensions/ErrorHandlingMiddleware.cs
+++ src/Services/EmailService/Microliu.EmailService/Extensions/ErrorHandlingMiddleware.cs
@@ -1,10 +1,12 @@
-﻿using Microliu.EmailService.API.Models;
+﻿using Microliu.Core.Loggers;
+using Microliu.EmailService.API.Models;
 using Microliu.EmailService.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Microliu.EmailService.API.Extensions
@@ -12,41 +14,47 @@ namespace Microliu.EmailService.API.Extensions
     public class ErrorHandlingMiddleware
     {
         private readonly RequestDelegate next;
-
-        public ErrorHandlingMiddleware(RequestDelegate next)
+        private readonly ILogger _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger logger)
         {
             this.next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
         {
             HttpResponse response = context.Response;
-
             try
             {
                 await next(context);
             }
             catch (EmailException emailException)
             {
-                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                response.StatusCode = (int)HttpStatusCode.OK;
                 response.ContentType = "application/json;charset=utf-8";
-                var result = new ReturnResult(response.StatusCode, emailException.Message);
+                var result = new ReturnResult(false, emailException.Message);
                 result.Data = string.Empty;
                 await HandleExceptionAsync(response, result);
             }
             catch (Exception ex)
             {
-                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.StatusCode = (int)HttpStatusCode.OK;
                 response.ContentType = "application/json;charset=utf-8";
-                var result = new ReturnResult(response.StatusCode, "AuthService异常，请稍后再试");
-                result.Data = ex.Message;
+                var result = new ReturnResult(false, "接口异常，请稍后再试");
+                result.Data = "";
+                _logger.ErrorBuilder($"[{DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [接口异常] [{ex.Message}]")
+                       .SetException(ex)
+                       .AddTags("error", "exception")
+                       .AddObject(result)
+                       .Submit();
+
                 await HandleExceptionAsync(response, result);
             }
         }
 
         private static async Task HandleExceptionAsync(HttpResponse response, ReturnResult result)
         {
-            await response.WriteAsync(JsonConvert.SerializeObject(result)).ConfigureAwait(false);
+            await response.WriteAsync(JsonConvert.SerializeObject(result), Encoding.UTF8);//.ConfigureAwait(false);
         }
 
     }
