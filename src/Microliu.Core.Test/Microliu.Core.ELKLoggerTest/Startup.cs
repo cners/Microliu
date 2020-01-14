e235694386386ca01b894628ea0dca403cc96991
LiuZhuang <liuzhuang@6iuu.com>
2020-01-14 19:59:42 +0800

    fix

--- src/Microliu.Core.Test/Microliu.Core.ELKLoggerTest/Startup.cs
+++ src/Microliu.Core.Test/Microliu.Core.ELKLoggerTest/Startup.cs
@@ -26,6 +26,7 @@ namespace Microliu.Core.ELKLoggerTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.addtransie
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
